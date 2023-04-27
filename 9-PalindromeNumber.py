fb345402ae190146d01a9c44aec52ed5c554c09a
kenneally15 <kenneally15@yahoo.com>
2023-04-26 21:58:51 -0700

    New 9 Palindrome solution

--- 9-PalindromeNumber.py
+++ 9-PalindromeNumber.py
@@ -25,4 +25,19 @@ class Solution:
         return True
 
 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        if x < 0 or (x%10 == 0 and x != 0): 
+            return False
+
+        reverse = 0
+        while x > reverse:
+            reverse = reverse*10 + x%10
+            x //= 10
+
+        print(x, reverse)
+        return (x == reverse or x == reverse//10)
+            
+
 
