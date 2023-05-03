ad46a2915d9529cb601428c8208ec8f84f9edb8a
kenneally15 <kenneally15@yahoo.com>
2023-05-02 21:45:17 -0700

    Recursive 14-LongestPrefix

--- 14-LongestPrefix.py
+++ 14-LongestPrefix.py
@@ -13,3 +13,30 @@ class Solution:
         
         return prefix
 
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        #Base Cases
+        if len(strs) == 0: return ""
+        if len(strs) == 1: return strs[0]
+
+        #Find Prefix
+        prefix = ""
+        if len(strs) == 2:
+            for i in range(min(len(strs[0]),len(strs[1]))):
+                if strs[0][i] != strs[1][i]:
+                    return prefix
+                prefix += strs[0][i]
+            return prefix
+        
+        #Recursion
+        mid = len(strs) // 2
+        left = self.longestCommonPrefix(strs[:mid])
+        right = self.longestCommonPrefix(strs[mid:])
+        
+        return self.longestCommonPrefix([left,right])
+
+
+
+            
