cfcea7dd86f9b9948e36a474518c5d9b21c5f5f1
kenneally15 <kenneally15@yahoo.com>
2023-05-03 23:05:48 -0700

    Recursive DFS 17-PhoneNumber

--- 17-PhoneNumber.py
+++ 17-PhoneNumber.py
@@ -31,4 +31,40 @@ class Solution:
 
 
 
+class Solution:
+    
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        dic = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        ans = []
+        if len(digits) == 0:
+            return ans
+
+        self.dfs(digits, "", ans, dic)
+
+        return ans
+
+
+    def dfs(self, digits: str, prefix: str, ans: List[str], dic):
+
+        #Base case
+        if len(digits) == 0:
+            ans.append(prefix)
+            return
         
+        #Recursive case
+        chars = dic[digits[0]]
+        for c in chars:
+            self.dfs(digits[1:], prefix+c, ans, dic)
+        
+    
