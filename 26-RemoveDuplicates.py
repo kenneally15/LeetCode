ffef7f62943d885944575f2ba0f02fa4b002e8fc
kenneally15 <kenneally15@yahoo.com>
2023-05-11 21:53:16 -0700

    26-RemoveDuplicates

---
+++ 26-RemoveDuplicates.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        insertIndex = 1
+
+        for i in range(1, len(nums)):
+            
+            if nums[insertIndex-1] != nums[i]:
+                nums[insertIndex] = nums[i]
+                insertIndex+=1
+            
+            #nums[i] = "_"
+        
+        return insertIndex
