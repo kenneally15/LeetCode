3a5703b0c7062b7688d798b8cef2e8f151a887fd
kenneally15 <kenneally15@yahoo.com>
2023-05-13 21:28:33 -0400

    27-RemoveElement

---
+++ 27-RemoveElement.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        insertIndex = 0
+
+        for i in range(len(nums)):
+
+            if nums[i] != val:
+                nums[insertIndex] = nums[i]
+                insertIndex+=1
+        
+        return insertIndex
+                
+            
+
+
+
