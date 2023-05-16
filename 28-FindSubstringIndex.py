8117ad7674f482da6ff0738044f6198391990152
kenneally15 <kenneally15@yahoo.com>
2023-05-15 23:08:44 -0700

    28-FindSubstringIndex

---
+++ 28-FindSubstringIndex.py
@@ -0,0 +1,16 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+
+        for i in range(len(haystack)-len(needle)+1):
+
+            j = 0
+            while j < len(needle) and needle[j] == haystack[i+j]:
+                j+=1
+            
+            if j == len(needle):
+                return i
+            
+        return -1
+            
+            
+            
