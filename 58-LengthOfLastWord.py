446166e1288e35eae88fbba8dbb17828fbecac51
kenneally15 <kenneally15@yahoo.com>
2023-06-26 21:09:00 -0700

    Problem #58

--- 58-LengthOfLastWord.py
+++ 58-LengthOfLastWord.py
@@ -1,7 +1,7 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
 
-        l = 0
+        length = 0
         
         i = len(s)-1
         while i>=0 and s[i] == ' ':
@@ -11,4 +11,4 @@ class Solution:
             length+=1
             i-=1
         
-        return l
+        return length
